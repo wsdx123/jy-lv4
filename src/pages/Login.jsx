da091d479933cb9b51cbface8e8bcddf4e23e7ae
Jun Young <50202150+wsdx123@users.noreply.github.com>
2023-07-08 20:04:31 +0900

    feat: login 예외처리 alert로

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,22 +2,44 @@ import axios from 'axios'
 import { useState } from 'react'
 
 function Login() {
-  const [userId, setUserId] = useState('')
-  const [password, setPassword] = useState('')
+  const [loginInfo, setLoginInfo] = useState({
+    userId: '',
+    password: ''
+  })
 
-  const handleRegister = (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault()
-    axios.post(`${process.env.REACT_APP_JWT_URL}/login`, {
-      id: userId,
-      password
-    })
+
+    const { userId, password } = loginInfo
+    if (userId === '' || password === '') {
+      alert('아이디 비번 입력해')
+      return
+    }
+
+    try {
+      await axios.post(`${process.env.REACT_APP_JWT_URL}/login`, {
+        id: userId,
+        password
+      })
+    } catch (error) {
+      const { message } = error.response.data
+      alert(message)
+    }
   }
 
   return (
     <div>
       <form onSubmit={handleRegister}>
-        <input type='text' value={userId} onChange={(e) => setUserId(e.target.value)} />
-        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input
+          type='text'
+          value={loginInfo.userId}
+          onChange={(e) => setLoginInfo({ ...loginInfo, userId: e.target.value })}
+        />
+        <input
+          type='password'
+          value={loginInfo.password}
+          onChange={(e) => setLoginInfo({ ...loginInfo, password: e.target.value })}
+        />
         <button type='submit'>로그인</button>
       </form>
     </div>
