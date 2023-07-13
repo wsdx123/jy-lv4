c19e2f429b02e909d5273c77d9ff0d7948057164
Jun Young <50202150+wsdx123@users.noreply.github.com>
2023-07-13 10:35:05 +0900

    feat: detail페이지 디자인, 게시물 수정 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from 'pages/Home'
 import Login from 'pages/Login'
 import Post from 'pages/Post'
 import Register from 'pages/Register'
+import Update from 'pages/Update'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
           <Route path='/detail/:postId' element={<Detail />} />
+          <Route path='/update/:postId' element={<Update />} />
         </Routes>
       </Layout>
     </BrowserRouter>
--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,13 +1,26 @@
 import { useQuery } from 'react-query'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { getPost } from 'service/api'
+import styles from 'pages/Detail/Detail.module.css'
 
 function Detail() {
   const params = useParams().postId
 
-  const { isLoading, isError, data } = useQuery(['post', params], () => getPost(params))
-  console.log(data)
-  return <div>Detail</div>
+  const { isLoading, data } = useQuery(['post', params], () => getPost(params))
+
+  if (isLoading) return <div>Loading...</div>
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>{data.title}</h2>
+      <img className={styles.picture} src={data.picture} alt={`picture_${data.id}`} />
+      <p>{data.context}</p>
+      <div className={styles.buttonBox}>
+        <Link to={`/update/${data.id}`}>수정</Link>
+        <button type='button'>삭제</button>
+      </div>
+    </div>
+  )
 }
 
 export default Detail
